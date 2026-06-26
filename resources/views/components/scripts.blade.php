e798d28155b0634ab818ab1aeaaaa4b5fa50cb6c
rzkyftrhmn <faturohmanrizky36@gmail.com>
2026-06-26 21:34:33 +0700

    blade templating admin

---
+++ resources/views/components/scripts.blade.php
@@ -0,0 +1,51 @@
+<!-- JQUERY JS -->
+<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+
+<!-- BOOTSTRAP JS -->
+<script src="{{ asset('assets/plugins/bootstrap/js/popper.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+
+<!-- SPARKLINE JS-->
+<script src="{{ asset('assets/js/jquery.sparkline.min.js') }}"></script>
+
+<!-- CHART-CIRCLE JS-->
+<script src="{{ asset('assets/js/circle-progress.min.js') }}"></script>
+
+<!-- CHARTJS CHART JS-->
+<script src="{{ asset('assets/plugins/chart/Chart.bundle.js') }}"></script>
+<script src="{{ asset('assets/plugins/chart/utils.js') }}"></script>
+
+<!-- PIETY CHART JS-->
+<script src="{{ asset('assets/plugins/peitychart/jquery.peity.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/peitychart/peitychart.init.js') }}"></script>
+
+<!-- INTERNAL SELECT2 JS -->
+<script src="{{ asset('assets/plugins/select2/select2.full.min.js') }}"></script>
+
+<!-- INTERNAL Data tables js-->
+<script src="{{ asset('assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatable/js/dataTables.bootstrap5.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatable/dataTables.responsive.min.js') }}"></script>
+
+<!-- ECHART JS-->
+<script src="{{ asset('assets/plugins/echarts/echarts.js') }}"></script>
+
+<!-- SIDE-MENU JS-->
+<script src="{{ asset('assets/plugins/sidemenu/sidemenu.js') }}"></script>
+
+<!-- SIDEBAR JS -->
+<script src="{{ asset('assets/plugins/sidebar/sidebar.js') }}"></script>
+
+<!-- Perfect SCROLLBAR JS-->
+<script src="{{ asset('assets/plugins/p-scroll/perfect-scrollbar.js') }}"></script>
+<script src="{{ asset('assets/plugins/p-scroll/pscroll.js') }}"></script>
+<script src="{{ asset('assets/plugins/p-scroll/pscroll-1.js') }}"></script>
+
+<!-- APEXCHART JS -->
+<script src="{{ asset('assets/js/apexcharts.js') }}"></script>
+
+<!-- INDEX JS -->
+<script src="{{ asset('assets/js/index1.js') }}"></script>
+
+<!-- CUSTOM JS -->
+<script src="{{ asset('assets/js/custom.js') }}"></script>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestCommitController;
 
 Route::get('/test-commit', [TestCommitController::class, 'index']);
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/dashboard', function () {
+    return view('dashboardAdmin');
 });
